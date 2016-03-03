eefea91e46977a6ed117dca4a9a7a17950217524
diastowo29 <diastowo@gmail.com>
2016-03-03 22:43:42 +0700

    adding TabActivity and fragment

---
+++ app/src/main/java/materialdesign/df29/lumiere/materialdesign/activity/OneFragment.java
@@ -0,0 +1,30 @@
+package materialdesign.df29.lumiere.materialdesign.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import materialdesign.df29.lumiere.materialdesign.R;
+
+/**
+ * Created by Lumiere on 3/3/2016.
+ */
+public class OneFragment extends Fragment {
+    public OneFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+}
